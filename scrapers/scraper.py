3aa29006f3723d229036fbd06093376e03dc9ebf
algentile <gogentileorgohome@gmail.com>
2018-09-16 20:11:38 -0400

    Can now get resource links for a particular page

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -1,71 +1,78 @@
 from bs4 import BeautifulSoup
+import time
 import pandas as pd
 import requests
-import ctime
 import re
+
 
 class Scraper(object):
 
     def __init__(self, webdriver='chrome'):
         self.webdriver = webdriver
 
-    
-    def convert_to_csv(filename, dict):
+    def convert_to_csv(self, filename, dict):
         df = pd.Dataframe.from_dict(dict, orient='index')
         return df.to_csv(filename + '{}', str(time.ctime()))
 
+
+"""
+Webscraper for autism speaks website
+"""
 class AutismSpeaksScraper(Scraper):
-    
+
     def __init__(self):
         self.site_url = 'https://www.autismspeaks.org/resource-guide/state/'
-        self.state_list = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
-          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
-          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
-          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
-          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-    
+        self.state_list = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+                           "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+                           "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+                           "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+                           "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
     """
-<<<<<<< HEAD
-    Process resources for each page 
+    Scrape for resource links on page and store in a dictionary of 
+    hyper link values.
+
+    State (str): The two letter character code for the US state.
     """
-    def process_state_page(self):
-        # for state in self.state_list:
-        prev_tag = None
+
+    def _process_state_page(self, state):
         href_dict = {}
-        resource_url = self.site_url + 'AL'
+        content_block = None
+        resource_url = self.site_url + state
         soup = BeautifulSoup(requests.get(resource_url).content)
-        resource_block = soup.find('div', class_='sharethis').find_next_sibling('ul')
+        resource_block = soup.find(
+            'div', class_='sharethis').find_next_sibling('ul')
+
         for tag in resource_block:
             next_tag = tag.find_next_sibling()
-            if(next_tag == 'li' and next_tag.find_next_sibling == 'ul'):
-                prev_tag = next_tag
-                href_dict[prev_tag] = ''
-            elif(tag == 'ul'):
-                for li in next_tag:
-                    print(li)
-                    href_dict[prev_tag] = li
+            if(tag.name == 'li' and next_tag.name == 'ul'):
+                content_block = tag.get_text()
+                href_dict[content_block] = []
+            if(tag.name == 'ul'):
+                for li_tag in tag.findAll('li'):
+                    li_tag_content = li_tag.find('a').get('href')
+                    href_dict[content_block].append(li_tag_content)
         return href_dict
+    
+    def process_test_page(self):
+        return self._process_state_page('AL')
+    
+    """
+    Grab the resource links for each state in
+    the state_list supported by Autism Speaks.
+    """
+
+    def get_resource_links(self):
+        resource_links = {}
+        for state in self.state_list:
+            href_dict = self._process_state_page(state)
+            resource_links[state] = href_dict
+            time.sleep(15) #Wait 15 seconds after each process request
+        return resource_links
+
 
 if __name__ == '__main__':
     x = AutismSpeaksScraper()
-    bs4_dict = x.process_state_page()
-    print(bs4_dict)
-=======
-    Converts Resource links to python dictionary of resource links
-    """
-    def process_state_page(self):
-        resource_dict = {}
-        for state in self.state_list:
-            soup = BeautifulSoup(requests.get(self.site_url + state).content)
-            for item in soup.find_all('a'):
-                    
-                
-                
-            
-
-
-if __name__ == '__main__':
-    x = AutismSpeaksScraper("", "")
-    x.process_state_page()
-            
->>>>>>> 7aeb3993ade06aa8f030cbfdbb9ac40bd5279440
+    href_dict = x.process_test_page()
+    print(href_dict)
+    
